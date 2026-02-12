d7eb0864c866574adc179b7afbf507308426c229
경문 <gyungmun.jeon@baligames.xyz>
2026-02-12 22:31:07 +0900

    * target text You've hit your limit -> Stop and wait for limit to

--- tmux_keep_alive.sh
+++ tmux_keep_alive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TARGET_TEXT="You've hit your limit"
+TARGET_TEXT="Stop and wait for limit to"
 
 tmux list-panes -a -F '#{session_name}:#{window_index}.#{pane_index}' | while read -r pane_id; do
     content=$(tmux capture-pane -pt "$pane_id" -S -15)
